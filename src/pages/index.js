beb8894cda687561077966c5a9c367b19d00f97b
kante-srikanth <kantesrikanth@gmail.com>
2020-10-13 11:25:18 +0530

    color changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -900,14 +900,16 @@ const IndexPage = () => (
         <div className="w-100">
           <h2 className="mb-5">Education</h2>
 
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-item d-flex flex-column flex-md-row justify-content-start mb-5">
             <div className="resume-content">
               <h3 className="mb-0">Vasavi College of Engineering</h3>
               <div className="subheading mb-3">Bachelor of Engineering</div>
               <div>Computer Science &amp; Engineering - 81%</div>
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">July 2013 - May 2017</span>
+              <b>
+                <span className="text-primary">July 2013 - May 2017</span>
+              </b>
             </div>
           </div>
 
