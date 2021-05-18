fc446268912aa0d01c0c83067de74b726df49067
Srikanth Kante <srikanth.kante@oracle.com>
2021-05-18 12:30:04 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cb3d738dd8e19f2d3556.js"
+    "url": "webpack-runtime-0c1e802389bea8f0fd58.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "62609ed080467427bee413db6d356fb7"
+    "revision": "98437d3d29c40a851ac14f818f69f709"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
